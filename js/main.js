d1734aa406a2f4f35bcfff20c4fd195e8f039082
francis <francisco.agustin.cabrera@gmail.com>
2022-11-07 11:15:32 -0300

    Segunda entrega JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,103 @@
+//objetos: Terapeutas
+const terapeuta1 = {
+  nombre: "Daniel Ortigoza",
+  edad: 49,
+  marco: "Terapia Cognitivo Conductual",
+  especialidad: "Ansiedad",
+  modalidad: "Virtual",
+  arancel: 2200
+}
+const terapeuta2 = {
+  nombre: "Maria Gonzalez",
+  edad: 22,
+  marco: "Terapia Cognitivo Conductual",
+  especialidad: "Ansiedad",
+  modalidad: "Virtual",
+  arancel: 1500
+}
+const terapeuta3 = {
+  nombre: "Francisco Cabrera",
+  edad: 29,
+  marco: "Terapia Cognitivo Conductual",
+  especialidad: "Ansiedad",
+  modalidad: "Virtual",
+  arancel: 3200
+}
+const terapeuta4 = {
+  nombre: "Camila Arconte",
+  edad: 65,
+  marco: "Terapia Cognitivo Conductual",
+  especialidad: "Depresión",
+  modalidad: "Virtual",
+  arancel: 2200
+}
+const terapeuta5 = {
+  nombre: "Gloria Rosales",
+  edad: 31,
+  marco: "Terapia Cognitivo Conductual",
+  especialidad: "Depresión",
+  modalidad: "Depresión",
+  arancel: 2000
+}
+const terapeuta6 = {
+  nombre: "Andres Cartero",
+  edad: 51,
+  marco: "Terapia Cognitivo Conductual",
+  especialidad: "Depresión",
+  modalidad: "Virtual",
+  arancel: 2500
+}
+const terapeuta7 = {
+  nombre: "Azul Rodriguez",
+  edad: 28,
+  marco: "Terapia Cognitivo Conductual",
+  especialidad: "Desarrollo Personal",
+  modalidad: "Virtual",
+  arancel: 2200
+}
+const terapeuta8 = {
+  nombre: "Pedro Sans",
+  edad: 26,
+  marco: "Terapia Cognitivo Conductual",
+  especialidad: "Desarrollo Personal",
+  modalidad: "Virtual",
+  arancel: 2700
+}
+const terapeuta9 = {
+  nombre: "Lourdes Fernandez",
+  edad: 49,
+  marco: "Terapia Cognitivo Conductual",
+  especialidad: "Desarrollo Personal",
+  modalidad: "Virtual",
+  arancel: 3000
+}
+const terapeuta10 = {
+  nombre: "Marta Martin",
+  edad: 49,
+  marco: "Terapia Cognitivo Conductual",
+  especialidad: "Desarrollo Personal",
+  modalidad: "Presencial",
+  arancel: 4200
+}
+const terapeuta11 = {
+  nombre: "Javier Minucin",
+  edad: 39,
+  marco: "Terapia Cognitivo Conductual",
+  especialidad: "Desarrollo Personal",
+  modalidad: "Presencial",
+  arancel: 4000
+}
+
+//array: Equipo
+const arrayEquipo = [terapeuta1, terapeuta2, terapeuta3, terapeuta4, terapeuta5, terapeuta6, terapeuta7, terapeuta8, terapeuta9, terapeuta10, terapeuta11];
+
+//Constructora en base a usuario
+function datosUsuario(nombre, edad, arancelPretendido, motivoConsulta){
+  this.nombre = nombre;
+  this.edad = edad;
+  this.motivoConsulta = motivoConsulta;
+}
+
 //funciones
 alert("Hola, Bienvenido/a a Psicogénero. Con esta web buscamos poder acercarte información sobre salud mental")
 
@@ -27,6 +127,12 @@ if (logueo) {
     switch (intereses) {
       case "1":
         alert("Te invitamos a visitar nuestra pestaña de contacto o enviar un mail a psicogenero@gmail.com");
+        const usuario1 = new datosUsuario(prompt("Nombre"),prompt("edad"),prompt("consulta"));
+        let presupuesto=parseInt(prompt("ingresá cuanto podés gastar"))
+        let resultado;
+        resultado= arrayEquipo.filter(profesional=>profesional.arancel < presupuesto)
+        console.log(resultado)
+        resultado.forEach (profesional => alert(usuario1.nombre + `El profesional ${profesional.nombre} tiene un arancel de $${profesional.arancel}`));
         break;
       case "2":
         let info = prompt("¿Busca información sobre la legalidad del ejercicio profesional? Escriba sí en caso de ser lo deseado");
@@ -49,3 +155,8 @@ if (logueo) {
 }
 
 alert("Gracias por visitar nuestra web y confiar en nosotros/as");
+
+//DOM
+finDePag.innerHTML = "ESTE TITULO REEMPLAZA A OTRO VIA JS --- Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus consequatur blanditiis consequuntur itaque eveniet provident voluptatibus aperiam maiores, non odit voluptas saepe quasi corporis at unde quo fugiat praesentium? Delectus!</h2>"
+finDePag.className = "letraTit";
+
